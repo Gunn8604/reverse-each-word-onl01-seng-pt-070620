be7857b0b0e9d38f39430ccfc3d6a773b61c42c5
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-17 02:04:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
-def reverse_each_word(sentence1)
-sentence1.reverse!
+def reverse_each_word(str)
+   array = str.split(" ")
+   array.collect do |word|
+      word.reverse! 
+   end
+   array.join(" ")
 end
